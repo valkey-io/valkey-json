f91f1b06439ba05439fe5e08c1b4adbe64f55dd5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-31 22:57:33 -0400

    spelling: ; otherwise,
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/rapidjson/document.h
+++ src/rapidjson/document.h
@@ -1039,7 +1039,7 @@ public:
     */
     GenericValue& operator=(GenericValue& rhs) RAPIDJSON_NOEXCEPT {
         if (RAPIDJSON_LIKELY(this != &rhs)) {
-            // Can't destroy "this" before assigning "rhs", otherwise "rhs"
+            // Can't destroy "this" before assigning "rhs"; otherwise, "rhs"
             // could be used after free if it's an sub-Value of "this",
             // hence the temporary danse.
             GenericValue temp;
